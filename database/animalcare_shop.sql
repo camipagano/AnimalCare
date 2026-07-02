c283cdd3b38c14f6d258123b65e14a19b7175a2f
giulisabatino <g.sabatino41@gmail.com>
2026-07-02 22:30:16 +0200

    Creazione database e struttura iniziale del progetto

---
+++ database/animalcare_shop.sql
@@ -0,0 +1,161 @@
+CREATE DATABASE  IF NOT EXISTS `animalcare_shop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `animalcare_shop`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: animalcare_shop
+-- ------------------------------------------------------
+-- Server version	8.4.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dettaglio_ordine`
+--
+
+DROP TABLE IF EXISTS `dettaglio_ordine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dettaglio_ordine` (
+  `Codice_Ordine` int NOT NULL,
+  `ID_Prodotto` int NOT NULL,
+  `Prezzo_Unitario` decimal(10,2) NOT NULL,
+  `Quantita` int NOT NULL,
+  PRIMARY KEY (`Codice_Ordine`,`ID_Prodotto`),
+  KEY `ID_Prodotto` (`ID_Prodotto`),
+  CONSTRAINT `dettaglio_ordine_ibfk_1` FOREIGN KEY (`Codice_Ordine`) REFERENCES `ordine` (`Codice`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `dettaglio_ordine_ibfk_2` FOREIGN KEY (`ID_Prodotto`) REFERENCES `prodotto` (`ID`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `metodo_di_pagamento`
+--
+
+DROP TABLE IF EXISTS `metodo_di_pagamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `metodo_di_pagamento` (
+  `Codice_Ordine` int NOT NULL,
+  `Stato` varchar(30) NOT NULL,
+  `Nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`Codice_Ordine`,`Nome`),
+  CONSTRAINT `metodo_di_pagamento_ibfk_1` FOREIGN KEY (`Codice_Ordine`) REFERENCES `ordine` (`Codice`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ordine`
+--
+
+DROP TABLE IF EXISTS `ordine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordine` (
+  `Codice` int NOT NULL AUTO_INCREMENT,
+  `Indirizzo` varchar(150) NOT NULL,
+  `Stato` varchar(30) NOT NULL,
+  `Data` date NOT NULL,
+  `ID_Utente` int NOT NULL,
+  PRIMARY KEY (`Codice`),
+  KEY `ID_Utente` (`ID_Utente`),
+  CONSTRAINT `ordine_ibfk_1` FOREIGN KEY (`ID_Utente`) REFERENCES `utente` (`ID`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `prodotto`
+--
+
+DROP TABLE IF EXISTS `prodotto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prodotto` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(100) NOT NULL,
+  `Descrizione` varchar(255) DEFAULT NULL,
+  `Disponibilita` int NOT NULL,
+  `Prezzo` decimal(10,2) NOT NULL,
+  `Quantita` int DEFAULT NULL,
+  `ID_Categoria` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_Categoria` (`ID_Categoria`),
+  CONSTRAINT `prodotto_ibfk_1` FOREIGN KEY (`ID_Categoria`) REFERENCES `categoria` (`ID`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recensione`
+--
+
+DROP TABLE IF EXISTS `recensione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recensione` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Voto` int NOT NULL,
+  `Commento` varchar(255) DEFAULT NULL,
+  `Data` date NOT NULL,
+  `ID_Utente` int NOT NULL,
+  `ID_Prodotto` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_Utente` (`ID_Utente`),
+  KEY `ID_Prodotto` (`ID_Prodotto`),
+  CONSTRAINT `recensione_ibfk_1` FOREIGN KEY (`ID_Utente`) REFERENCES `utente` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recensione_ibfk_2` FOREIGN KEY (`ID_Prodotto`) REFERENCES `prodotto` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recensione_chk_1` CHECK ((`Voto` between 1 and 5))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utente` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(50) NOT NULL,
+  `Cognome` varchar(50) NOT NULL,
+  `Mail` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Ruolo` varchar(50) DEFAULT NULL,
+  `Indirizzo` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Mail` (`Mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-02 22:20:35
